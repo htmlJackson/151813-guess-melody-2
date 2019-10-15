eafd46aa13d2d279f0a91c1d31f0a5c1ef4c4849
Evgeny Ber <fiend.jackson@gmail.com>
2019-10-15 17:34:28 +0300

    форматирование кода

--- src/index.js
+++ src/index.js
@@ -2,16 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 const init = () => {
-  const settings = {
-    gameTime: 5,
-    errorCount: 3,
-  };
-
   ReactDOM.render(
-    <App
-      errorCount={settings.errorCount}
-      gameTime={settings.gameTime}
-    />,
+    <App />,
     document.querySelector(`#root`)
   );
 };
